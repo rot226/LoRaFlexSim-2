48a94f1b83bcb64f5aa84646ecc7b88f1d0b5ae8
rot226 <160727202+rot226@users.noreply.github.com>
2026-04-17 20:32:08 +0000

    Add Thompson SF selector with UCB-compatible API

--- loraflexsim/learning/__init__.py
+++ loraflexsim/learning/__init__.py
@@ -1,5 +1,6 @@
 """Composants d'apprentissage pour LoRaFlexSim."""
 
+from .thompson import LoRaSFSelectorThompson
 from .ucb1 import LoRaSFSelectorUCB1, UCB1Bandit
 
-__all__ = ["LoRaSFSelectorUCB1", "UCB1Bandit"]
+__all__ = ["LoRaSFSelectorUCB1", "LoRaSFSelectorThompson", "UCB1Bandit"]
